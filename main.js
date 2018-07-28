b4bd2775cc7341a9d4c201c49ffdc3c6552a33fb
maharshi365 <maharshi365@gmail.com>
2018-07-28 14:48:51 -0400

    made functions to simplify code

--- main.js
+++ main.js
@@ -1,15 +1,71 @@
-function processRawData(){
-	rawData = document.getElementById("inputText").value
-	console.log(rawData);
+$(document).ready(function(){
+	$("#gobutton").click(function() {
+		var userText = $("#inputText").val();
+		var data = processRawData(userText);
+		data = getAllData(data);
+		data = data.sort()
+		
+		// extract all unique years
+		var years = [];
+		years = extractUniqueYears(years,data);
+
+		// extract all unique semesters
+		var semsetersUn = [];
+		semsetersUn = extractUniqueSemesters(semsetersUn,data);
+
+		var cumulative = [];
+		cumulative = calculateCumulativeGPA(cumulative,data);
+
+		var yearly = [];
+		yearly = calculateYearlyGPA(yearly,data,years);
+
+		var semesterly = [];
+		semseterly = calculateSemesterlyGPA(semesterly,data,semsetersUn);
+
+		console.log(cumulative);
+		console.log(yearly);
+		console.log(semseterly);
+
+		tableString = '<table>' + createTableHeaders();
+		
+		for(var i = 0; i<yearly.length; i++){
+			tableString = tableString + createYearRow(yearly[i][0],yearly[i][1],yearly[i][2],yearly[i][3]);
+			for(var j = 0; j<semseterly.length; j++){
+				if(semseterly[j][4] == yearly[i][4]){
+					tableString = tableString + createSemesterRow(semseterly[j][0],semseterly[j][1],semseterly[j][2],semseterly[j][3]);
+				}
+			}
+		}
+		console.log(tableString);
+		tableString = tableString + '</tbody></table>';
+		$("#gradesTable").append(tableString);
+	});
+
+});
+
+
+
+function getAllData(data){
+	data = getTwelvePoint(data);
+	data = isIncluded(data);
+	data = addAcademicYear(data);
+	data = addSemester(data);
+	data = calculateCredits(data)
+	data = convertUnits(data);
+	return data;	
+}
+
+function processRawData(rawData){
 	var format = /[A-Z]+ +(.*)+\n+(.*)+\n+(.*)+\n+(.*)+\n+(.*)+\n+(.*)+/gm;
-	
 	var courses = [];
-	var years = [];
 	while((row = format.exec(rawData)) !== null){
 		var entry = row[0].split('\n');
 		courses.push(entry);
 	}
+	return courses;
+}
 
+function getTwelvePoint(courses){
 	for (var i=0;i<courses.length;i++){
 		// get the Twelve Point grade from the letter grade
 		var grade = courses[i][3];
@@ -52,35 +108,98 @@ function processRawData(){
 		else{
 			courses[i].push(0);
 		}
+	}
 
-		// Check to see if course should be included in GPA Calc
+	return courses;
+}
+
+function isIncluded(courses){
+	for (var i=0;i<courses.length;i++){
 		if((courses[i][5] == 'In Progress') || (courses[i][3] == 'NC') || (courses[i][3] == 'COM') || (courses[i][5] == 'Transferred')){
-			courses[i].push('False');
+			courses[i].push(false);
 		}
 		else{
-			courses[i].push('True');
+			courses[i].push(true);
 		}
+	}
 
-		// calculate academic year of course taken
+	return courses;
+}
+
+function addAcademicYear(courses){
+	for (var i=0;i<courses.length;i++){
 		if(courses[i][2].substring(5,9) == 'Fall'){
 			courses[i].push(parseInt(courses[i][2].substring(0,4)));
-			years.push(parseInt(courses[i][2].substring(0,4)));
 		}
 		else{
 			courses[i].push(parseInt(courses[i][2].substring(0,4)) - 1);
-			years.push(parseInt(courses[i][2].substring(0,4)) - 1);
 		}
-
-		// calculate credits earned
-		courses[i].push(courses[i][4]*courses[i][6]);
 	}
 
-	//extract all unique years from data
-	years = Array.from(new Set(years));
-	
-	//final data to return
-	var finalData = [];
+	return courses;
+}
 
+function addSemester(courses){
+	for (var i=0;i<courses.length;i++){
+		var semsester = courses[i][2].split(' ');
+		courses[i].push(semsester[1]);
+	}
+	return courses;
+}
+
+function extractUniqueYears(years,courses){
+	for (var i=0;i<courses.length;i++){
+		if(courses[i][2].substring(5,9) == 'Fall'){
+			years.push(parseInt(courses[i][2].substring(0,4)));
+		}
+		else{
+			years.push(parseInt(courses[i][2].substring(0,4)) - 1);
+		}
+	}
+
+	years = Array.from(new Set(years));
+	return years;
+}
+
+function extractUniqueSemesters(semesters,courses){
+	for (var i=0;i<courses.length;i++){
+		semesters.push(courses[i][2]);
+	}
+	semesters = Array.from(new Set(semesters));
+	semseters = semesters.sort();
+	return semesters;
+}
+
+function calculateCredits(courses){
+	for (var i=0;i<courses.length;i++){
+		courses[i].push(courses[i][4]*courses[i][6]);
+	}
+	return courses;
+}
+
+function convertUnits(courses){
+	for (var i=0;i<courses.length;i++){
+		courses[i][4] = parseFloat(courses[i][4]);
+	}
+	return courses;
+}
+
+function calculateCumulativeGPA(cumulative,courses){
+	//calculate final gpa
+	totalCredits = 0;
+	totalUnits = 0.0;
+	for (var l = 0; l<courses.length; l++){
+		if(courses[l][7] == true){
+		totalUnits = totalUnits + parseInt(courses[l][4]);
+		totalCredits = totalCredits + courses[l][10];
+	}
+}
+	cumulative=['Cumulative',totalUnits.toString(),totalCredits.toString(),((totalCredits/totalUnits).toFixed(2)).toString()];
+	return cumulative;
+}
+
+function calculateYearlyGPA(yearly,courses,years){
+	var finalData = [];
 	//calculate yearly gpa
 	var totalCredits = 0;
 	var totalUnits = 0.0;
@@ -89,46 +208,76 @@ function processRawData(){
 		totalCredits = 0;
 		totalUnits = 0;
 		for(var k = 0; k<courses.length; k++){
-			if(courses[k][8] == years[j]){
+			if((courses[k][8] == years[j]) && courses[k][7] == true){
 				totalUnits = totalUnits + parseInt(courses[k][4]);
-				totalCredits = totalCredits + courses[k][9];
+				totalCredits = totalCredits + courses[k][10];
 			}
 		}
-		finalData[j]=[years[j].toString(),totalUnits.toString(),totalCredits.toString(),((totalCredits/totalUnits).toFixed(2)).toString()];
+		finalData[j]=[years[j].toString() + '-' + (years[j]+1).toString(),totalUnits.toString(),totalCredits.toString(),((totalCredits/totalUnits).toFixed(2)).toString()];
 
 	}
 
-	//calculate final gpa
-	totalCredits = 0;
-	totalUnits = 0.0;
-	for (var l = 0; l<courses.length; l++){
-		if(courses[l][7] == 'True'){
-		totalUnits = totalUnits + parseInt(courses[l][4]);
-		totalCredits = totalCredits + courses[l][9];
+	for(var i = 0; i<finalData.length; i++){
+		finalData[i].push(parseInt(finalData[i][0]));
 	}
+
+	yearly = finalData;
+	return yearly;
 }
-	finalData[j]=['Cumulative',totalUnits.toString(),totalCredits.toString(),((totalCredits/totalUnits).toFixed(2)).toString()];
 
-	//add headers and table rows to output table
-	var rows = finalData.length;
-	var columns = finalData[0].length;
-	var table = document.getElementById("mytable");
-	var tableHeaders = ['Year','Units Taken', 'Credits Earned', '12 Point GPA'];
-	
-	// add table data
-	for(var r = 0; r<rows; r++){
-		var row = table.insertRow(r);
-		for(var c = 0; c<columns; c++){
-			var cell = row.insertCell(c);
-			cell.innerHTML = finalData[r][c];
+function calculateSemesterlyGPA(semesterly,courses,semesters){
+	var finalData = [];
+	//calculate yearly gpa
+	var totalCredits = 0;
+	var totalUnits = 0.0;
+
+	for (var j = 0; j<semseters.length; j++){
+		totalCredits = 0;
+		totalUnits = 0;
+		for(var k = 0; k<courses.length; k++){
+			if((courses[k][2] == semesters[j]) && courses[k][7] == true){
+				totalUnits = totalUnits + parseInt(courses[k][4]);
+				totalCredits = totalCredits + courses[k][10];
+			}
+		}
+		finalData[j]=[semesters[j],totalUnits.toString(),totalCredits.toString(),((totalCredits/totalUnits).toFixed(2)).toString()];
+
+	}
+
+	finalData = academicYearID(finalData);
+
+	semesterly = finalData.sort();
+	return semesterly;
+}
+
+
+function academicYearID(data){
+	for (var i = 0;i<data.length;i++){
+		if (data[i][0].substring(5,9) == 'Fall'){
+			data[i].push(parseInt(data[i][0].substring(0,4)));
+		}
+		else{
+			data[i].push(parseInt(data[i][0].substring(0,4))-1);	
 		}
 	}
+	return data;
+}
 
-	//add table headers
-	var row = table.insertRow(0);
-	for(var n = 0; n<columns; n++){
-		var cell = row.insertCell(n);
-		cell.innerHTML = tableHeaders[n];
-	}
+function createTableHeaders(){
+	return '<thead><tr class="tableheader"><th>Time Period</th><th>Units Taken</th><th>Credits Earned</th><th>GPA</th></tr></thead><tbody>'
+}
+
+function createYearRow(TimePeriod,Units,Credits,grade){
+	return '<tr class="breakrow"><td>' + TimePeriod.toString() + '</td><td>' + Units.toString() + '</td><td>' + Credits.toString() + '</td><td>' + grade.toString() + '</td></tr>';
 
 }
+
+function createSemesterRow(TimePeriod,Units,Credits,grade){
+	return '<tr class="datarow"><td>' + TimePeriod.toString() + '</td><td>' + Units.toString() + '</td><td>' + Credits.toString() + '</td><td>' + grade.toString() + '</td></tr>';
+}
+
+
+$('.first-level').click(function() {
+    $(this).nextUntil('.first-level').toggle("slow");
+   
+});
